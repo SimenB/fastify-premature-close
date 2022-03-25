bbce44d5df16cb8dbb34131fdea979c852fdd1c8
Simen Bekkhus <sbekkhus91@gmail.com>
2022-03-25 16:03:52 +0100

    initial

---
+++ index.js
@@ -0,0 +1,35 @@
+import { createReadStream } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import contentDisposition from 'content-disposition';
+import Fastify from 'fastify';
+import fastifyCompress from 'fastify-compress';
+import fastifyStatic from 'fastify-static';
+
+const fastify = Fastify();
+
+fastify.register(fastifyCompress);
+fastify.register(fastifyStatic, {
+  root: dirname(fileURLToPath(import.meta.url)),
+});
+
+fastify.get('/', (_request, reply) => {
+  reply.type('text/html').send('<a href="/image.jpeg">Download</a>');
+});
+
+fastify.get('/image.jpeg', (_request, reply) => {
+  const filename = './oslo_ast_2000239_lrg.jpeg';
+  reply
+    .header('content-disposition', contentDisposition(filename))
+    .compress(createReadStream(filename));
+});
+
+fastify.setErrorHandler((error, _request, reply) => {
+  console.error(error);
+
+  reply.status(500).send();
+});
+
+await fastify.listen(8081);
+
+console.log('Open http://localhost:8081');
